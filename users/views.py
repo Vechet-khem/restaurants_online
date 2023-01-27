d4c7a82c81ed6a34bd93bf1cf0159f367e79fa15
satrotana <satrotana@gmail.com>
2023-01-27 21:46:16 +0700

    user post

--- users/views.py
+++ users/views.py
@@ -4,10 +4,17 @@ from rest_framework.response import Response
 from .models import *
 from .serializer import *
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def user(request):
     if request.method == 'GET':
         obj = User.objects.all()
         serializer = UserSerializer(obj, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = UserDetailSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            newObj = User.objects.get(id = serializer.data['id'])
+            serializer1 = UserSerializer(newObj)
+            return Response(serializer1.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
